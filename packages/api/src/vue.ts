5de486c5726f868eccff53b638b69bce91d1ba65
zcf0508 <zcf0508@live.com>
2024-04-30 14:37:16 +0800

    chore: update volar

--- packages/api/src/vue.ts
+++ packages/api/src/vue.ts
@@ -1,19 +1,20 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import type * as ts from "typescript/lib/tsserverlibrary"
-import { type FileRegistry } from "@volar/language-core"
+import { type Language } from "@volar/language-core"
 import { proxyCreateProgram } from "@volar/typescript"
 import {
+    VueCompilerOptions,
     createParsedCommandLine,
     createVueLanguagePlugin,
+    resolveVueCompilerOptions,
 } from "@vue/language-core"
 import * as SourceMaps from "@volar/source-map"
-import { createFakeGlobalTypesHolder } from "vue-tsc"
 import { SourceFileLocation, TypescriptContext } from "./types"
 
 const windowsPathReg = /\\/g
 
-// https://github.com/volarjs/volar.js/blob/v2.1.0/packages/typescript/lib/node/proxyCreateProgram.ts#L139
-type VuePrograme = ts.Program & { __volar__?: { files: FileRegistry } }
+// https://github.com/volarjs/volar.js/blob/v2.2.0-alpha.12/packages/typescript/lib/node/proxyCreateProgram.ts#L209
+type VuePrograme = ts.Program & { __volar__?: { language: Language } }
 
 let oldPrograme: VuePrograme | undefined
 
@@ -26,7 +27,7 @@ export function getPositionOfLineAndCharacterForVue(
 
     const compilerOptions = {
         ...ctx.program.getCompilerOptions(),
-        sourceMap: true,
+        rootDir: ctx.program.getCurrentDirectory(),
         declaration: true,
         emitDeclarationOnly: true,
         allowNonTsExtensions: true,
@@ -39,61 +40,59 @@ export function getPositionOfLineAndCharacterForVue(
         oldProgram: oldPrograme || ctx.program,
     }
 
+    let vueOptions: VueCompilerOptions
     const createProgram = proxyCreateProgram(
         ctx.ts,
         ctx.ts.createProgram,
-        [".vue"],
         (ts, _options) => {
             const { configFilePath } = _options.options
-            const vueOptions =
+            vueOptions =
                 typeof configFilePath === "string"
                     ? createParsedCommandLine(
                           ts,
                           ts.sys,
                           configFilePath.replace(windowsPathReg, "/")
                       ).vueOptions
-                    : {}
+                    : resolveVueCompilerOptions({
+                          extensions: [".vue", ".cext"],
+                      })
             return [
                 createVueLanguagePlugin(
                     ts,
                     (id) => id,
+                    _options.host?.useCaseSensitiveFileNames?.() ?? false,
+                    () => "",
+                    () =>
+                        _options.rootNames.map((rootName) =>
+                            rootName.replace(windowsPathReg, "/")
+                        ),
                     _options.options,
-                    vueOptions,
-                    true,
-                    createFakeGlobalTypesHolder(options)?.replace(
-                        windowsPathReg,
-                        "/"
-                    )
+                    vueOptions
                 ),
             ]
         }
     )
 
-    // @ts-expect-error use `ctx.pargram` first
-    if (ctx.program?.__volar__) {
-        oldPrograme = ctx.program
-    } else if (!oldPrograme?.__volar__) {
+    oldPrograme = oldPrograme ?? ctx.program
+
+    if (!oldPrograme?.__volar__) {
+        console.log("create vue program")
         oldPrograme = createProgram(options) as VuePrograme
     }
 
-    const sourceFile = oldPrograme.getSourceFile(fileName)
-
-    if (sourceFile) {
-        if (oldPrograme.__volar__) {
-            const vFile = oldPrograme.__volar__.files.get(fileName)
-
-            if (
-                vFile?.generated?.code &&
-                vFile?.generated?.code.languageId === "vue"
-            ) {
-                const code = vFile?.generated?.code?.embeddedCodes?.[0]
-                if (code) {
-                    const sourceMap = new SourceMaps.SourceMap(code.mappings)
-                    startPos =
-                        (sourceMap.getGeneratedOffset(startPos)?.[0] || -1) +
-                        // https://github.com/volarjs/volar.js/blob/v2.1.0/packages/typescript/lib/node/proxyCreateProgram.ts#L84
-                        (vFile?.generated?.code?.snapshot?.getLength() || 0)
-                }
+    if (oldPrograme.__volar__?.language.scripts) {
+        const vFile = oldPrograme.__volar__.language.scripts.get(fileName)
+        if (
+            vFile?.generated?.root &&
+            vFile?.generated?.root.languageId === "vue"
+        ) {
+            const code = vFile?.generated?.root?.embeddedCodes?.[0]
+            if (code) {
+                const sourceMap = new SourceMaps.SourceMap(code.mappings)
+                startPos =
+                    (sourceMap.getGeneratedOffset(startPos)?.[0] || -1) +
+                    // https://github.com/volarjs/volar.js/blob/v2.2.0-alpha.12/packages/typescript/lib/node/proxyCreateProgram.ts#L143
+                    (vFile?.generated?.root?.snapshot?.getLength() || 0)
             }
         }
     }
